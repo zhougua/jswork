67c03cf4af8098d522b81dda5c2da62b4445e05e
zhougua <1454786384@qq.com>
2019-10-16 13:26:18 +0800

    sannci

--- work4/work4.js
+++ work4/work4.js
@@ -1,7 +1,7 @@
 document.write("<p>金字塔</p>")
 let level =prompt('请设置金字塔的层数')
 level = parseFloat(level)&&Number(level)
-if (isNaN(levle)){
+if (isNaN(level)){
     alert('金字塔的层数是数字')
 }
 for(let i=1;i<=level;++i){
@@ -19,3 +19,14 @@ for(let i=1;i<=level;++i){
 }
 
 let str = '<table border="1">'
+for(let i=1;i<10;++i){
+    str += '<tr>'
+    for(var j=1;j<=i;++j){
+        //拼接单元格
+        str += '<td>' + j + '*' + i + '=' + ( j * i ) + '<td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+//设置div的html文档内容
+document.getElementById('table').innerHTML = str
