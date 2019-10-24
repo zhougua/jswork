72d88ac18019ae7ac60d217b656cb1b212da50c6
zhougua <1454786384@qq.com>
2019-10-24 12:15:15 +0800

    xiugai

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -50,13 +50,13 @@ for (const key in row) {
 showElement.innerHTML = str
 //如果定位指针不是-1（-1是最后一条记录），就调用延时方法调用slideShow函数显示下一条记录以形成动画效果
     if (pos[0] != -1)
-        setTimeout("showLog(logValues,showDiv)",200)//^毫秒，即运行快慢
+        setTimeout("showLog(logValues,showDiv)",500)//^毫秒，即运行快慢
 }
 
 //插入排序
-function insertSort(str) {
+function insertSort(str2) {
     //请自行完成插入排序用于动画演示的代码，以完成挑战任务
-    let arr = str.split(',')
+    let arr = str2.split(',')
     //声明排序步骤记录数组sortLog数组的数据用于动画演示
     let sortLog = []
 
@@ -66,14 +66,14 @@ function insertSort(str) {
     for(let i = 1; i < arr.length; i++){
         for(let j = i; j > 0; j--) {
             //把比较前的数组状态和定位指针保存到sortLog中，为保持演示效果，多加两行（可不加，演示速度会变快）
-            sortLog.push([arr.concat(), [j, j + 1]])
-            sortLog.push([arr.concat(), [j, j + 1]])
-            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j - 1, j]])
+            sortLog.push([arr.concat(), [j - 1, j]])
+            sortLog.push([arr.concat(), [j - 1, j]])
             
             if (arr[j - 1] > arr[j]){
                 [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
                  //把比较之后的数组状态和定位指针保存到sortLog中
-                 sortLog.push([arr.concat(), [j, j + 1]])
+                 sortLog.push([arr.concat(), [j - 1, j]])
             }
         }
     }
@@ -85,7 +85,7 @@ function insertSort(str) {
     //
     //显示排序的每一个步骤
 function showLog(logValues, showElement) {
-    let str = ''
+    let str2 = ''
     //解构赋值的方法，从排序日志遍历器去取出一条结果，赋值给row和pos变量
     let{
         done,
@@ -99,12 +99,12 @@ for (const key in row) {
     //根据定位指针数据把进行比较的数字变成红色
     let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
     //设置字体大小等html代码
-    str += '<span style="font-size:' + row[key] * 20 + 'px;' + color + '">' + row[key] + '</span>'
+    str2 += '<span style="font-size:' + row[key] * 20 + 'px;' + color + '">' + row[key] + '</span>'
 }
 //把html代码设置到现实的div中
-showElement.innerHTML = str
+showElement.innerHTML = str2
 //如果定位指针不是-1（-1是最后一条记录），就调用延时方法调用slideShow函数显示下一条记录以形成动画效果
     if (pos[0] != -1)
-        setTimeout("showLog(logValues,showDiv)",200)//^毫秒，即运行快慢
+        setTimeout("showLog(logValues,showDiv)",500)//^毫秒，即运行快慢
 }
 }
